0ece6d9b2952c22eef3ad0fb3ac01616ead1e86d
Bayhaqiindra <bayhaqi30072004.com>
2024-12-29 00:36:15 +0700

    Menambahkan objek DestinasiHome untuk navigasi dan penamaan rute home

--- app/src/main/java/com/example/pertemuan12/ui/view/HomeView.kt
+++ app/src/main/java/com/example/pertemuan12/ui/view/HomeView.kt
@@ -1,2 +1,8 @@
 package com.example.pertemuan12.ui.view
 
+import com.example.pertemuan12.navigation.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = "Home Mhs"
+}
