cfce43659f22a13645abf8566dfb3d63a8103ab0
Bayhaqiindra <bayhaqi30072004.com>
2024-12-28 23:30:13 +0700

    Menambahkan konversi data Mahasiswa ke InsertUiEvent

---
+++ app/src/main/java/com/example/pertemuan12/ui/viewmodel/InsertViewModel.kt
@@ -0,0 +1,11 @@
+import com.example.pertemuan12.model.Mahasiswa
+
+
+fun Mahasiswa.toInsertUiEvent(): InsertUiEvent = InsertUiEvent(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
