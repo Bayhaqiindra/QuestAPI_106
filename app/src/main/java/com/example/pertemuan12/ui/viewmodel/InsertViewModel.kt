15e8c0b6239b1820bf8bd76d6d5bc4c621b53592
Bayhaqiindra <bayhaqi30072004.com>
2024-12-28 23:33:58 +0700

    Menambahkan data class InsertUiEvent dan konversi ke Mahasiswa

--- app/src/main/java/com/example/pertemuan12/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/pertemuan12/ui/viewmodel/InsertViewModel.kt
@@ -1,5 +1,23 @@
 import com.example.pertemuan12.model.Mahasiswa
 
+data class InsertUiEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val jenisKelamin: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
+
+fun InsertUiEvent.toMhs(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
+
 fun Mahasiswa.toUiStateMhs(): InsertUiState = InsertUiState(
     insertUiEvent = toInsertUiEvent()
 )
