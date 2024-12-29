d214656c42d5e0a910727ec06810413ff42c073f
Bayhaqiindra <bayhaqi30072004.com>
2024-12-30 00:25:51 +0700

    Tambah UI untuk form input dan tombol update mahasiswa

--- app/src/main/java/com/example/pertemuan12/ui/view/UpdateView.kt
+++ app/src/main/java/com/example/pertemuan12/ui/view/UpdateView.kt
@@ -1,17 +1,23 @@
 package com.example.pertemuan12.ui.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
 import com.example.pertemuan12.navigation.DestinasiNavigasi
 import com.example.pertemuan12.ui.customwidget.CostumeTopAppBar
 import com.example.pertemuan12.ui.viewmodel.PenyediaViewModel
@@ -75,6 +81,32 @@ fun UpdateBody(
     onSaveClick: () -> Unit,
     modifier: Modifier = Modifier
 ){
-
+    Column(
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ) {
+        FormInput(
+            updateUiEvent = updateUiState.updateUiEvent,
+            onValueChange = onSiswaValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Update")
+        }
+    }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInput(
+    updateUiEvent: UpdateUiEvent,
+    modifier: Modifier = Modifier,
+    onValueChange: (UpdateUiEvent) -> Unit = {},
+    enabled: Boolean = true
+) {
+
+}
