d3d43d23bd2324545c33e00e415d0d3f97f07099
Bayhaqiindra <bayhaqi30072004.com>
2024-12-30 00:29:20 +0700

    Tambahkan sealed class DetailUiState untuk representasi status UI

--- app/src/main/java/com/example/pertemuan12/ui/view/DetailView.kt
+++ app/src/main/java/com/example/pertemuan12/ui/view/DetailView.kt
@@ -1,1 +1,9 @@
 package com.example.pertemuan12.ui.view
+
+import com.example.pertemuan12.model.Mahasiswa
+
+sealed class DetailUiState {
+    object Loading : DetailUiState()
+    data class Success(val mahasiswa: Mahasiswa) : DetailUiState()
+    object Error : DetailUiState()
+}
