e85daeed5db7e8f03383e6cc6c2d948af8076131
Bayhaqiindra <bayhaqi30072004.com>
2024-12-30 00:13:44 +0700

    fixed

--- app/src/main/java/com/example/pertemuan12/ui/viewmodel/UpdateViewModel.kt
+++ app/src/main/java/com/example/pertemuan12/ui/viewmodel/UpdateViewModel.kt
@@ -28,12 +28,13 @@ class UpdateViewModel(private val mhs: MahasiswaRepository) : ViewModel() {
         uiState = UpdateUiState(updateUiEvent = updateUiEvent)
     }
 
-suspend fun updateMahasiswa(nim: String) {
-    viewModelScope.launch {
-        try {
-            mhs.updateMahasiswa(nim, uiState.updateUiEvent.toMahasiswa())
-        } catch (e: Exception) {
-            e.printStackTrace()
+    suspend fun updateMahasiswa(nim: String) {
+        viewModelScope.launch {
+            try {
+                mhs.updateMahasiswa(nim, uiState.updateUiEvent.toMahasiswa())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
         }
     }
 }
