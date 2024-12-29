86399e7168553c5b95dd262d73df349203ca2144
Bayhaqiindra <bayhaqi30072004.com>
2024-12-29 23:30:18 +0700

    Menambahkan inisialisasi DetailViewModel dan UpdateViewModel

--- app/src/main/java/com/example/pertemuan12/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pertemuan12/ui/viewmodel/PenyediaViewModel.kt
@@ -1,16 +1,18 @@
-package com.example.pertemuan12
+package com.example.pertemuan12.ui.viewmodel
 
 import InsertViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
-import com.example.pertemuan12.ui.viewmodel.HomeViewModel
+import com.example.pertemuan12.MahasiswaApplications
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer { HomeViewModel(aplikasiMahasiswa().container.mahasiswaRepository) }
         initializer { InsertViewModel(aplikasiMahasiswa().container.mahasiswaRepository) }
+        initializer { DetailViewModel(aplikasiMahasiswa().container.mahasiswaRepository) }
+        initializer { UpdateViewModel(aplikasiMahasiswa().container.mahasiswaRepository) }
     }
     fun CreationExtras.aplikasiMahasiswa(): MahasiswaApplications =
         (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApplications)
