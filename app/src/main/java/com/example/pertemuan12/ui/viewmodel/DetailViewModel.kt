b16d612bbf2a1ae0ed549c2fb20555a6a12da4f8
Bayhaqiindra <bayhaqi30072004.com>
2024-12-30 14:07:00 +0700

    menambah fungsi delete dan fixed function get

--- app/src/main/java/com/example/pertemuan12/ui/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/pertemuan12/ui/viewmodel/DetailViewModel.kt
@@ -7,31 +7,39 @@ import com.example.pertemuan12.model.Mahasiswa
 import com.example.pertemuan12.repository.MahasiswaRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import java.io.IOException
 
 sealed class DetailUiState {
     object Loading : DetailUiState()
     data class Success(val mahasiswa: Mahasiswa) : DetailUiState()
-    object Error : DetailUiState()
+    data class Error(val message: String) : DetailUiState()
 }
 
-class DetailViewModel(private val repository: MahasiswaRepository) : ViewModel() {
+class DetailViewModel(private val mhs: MahasiswaRepository) : ViewModel() {
     private val _uiState = MutableStateFlow<DetailUiState>(DetailUiState.Loading)
-    val uiState: StateFlow<DetailUiState> = _uiState
+    val uiState: StateFlow<DetailUiState> = _uiState.asStateFlow()
 
     fun getMahasiswaById(nim: String) {
         viewModelScope.launch {
-            _uiState.value = DetailUiState.Loading
             try {
-                val mahasiswa = repository.getMahasiswaById(nim)
+                val mahasiswa = mhs.getMahasiswaById(nim)
                 _uiState.value = DetailUiState.Success(mahasiswa)
-            } catch (e: IOException) {
-                e.printStackTrace()
-                _uiState.value = DetailUiState.Error
-            } catch (e: HttpException) {
-                e.printStackTrace()
-                _uiState.value = DetailUiState.Error
+            } catch (e: Exception) {
+                _uiState.value = DetailUiState.Error(e.localizedMessage ?: "Terjadi kesalahan.")
+            }
+        }
+    }
+
+
+    fun deleteMhs(nim: String) {
+        viewModelScope.launch {
+            try {
+                mhs.deleteMahasiswa(nim)
+                _uiState.value = DetailUiState.Error("Data Mahasiswa telah dihapus.")
+            } catch (e: Exception) {
+                _uiState.value = DetailUiState.Error(e.localizedMessage ?: "Gagal menghapus data.")
             }
         }
     }
